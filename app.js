5f4504e75accd076e29a985692137eb440f6a563
cyrus8050 <ckmobile8050@gmail.com>
2020-11-12 23:12:04 +0800

    create the put request and update form

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Item = require('./models/items');
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
 const mongodb = 'mongodb+srv://ckmobile:ckmobile123@cluster0.niuuw.mongodb.net/item-database?retryWrites=true&w=majority';
 mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('connected')
@@ -27,6 +29,34 @@ app.get('/get-items', (req, res) => {
 })
 app.get('/add-item', (req, res) => {
     res.render('add-item');
+})
+
+app.post('/items', (req, res) => {
+    console.log(req.body)
+    const item = Item(req.body);
+    item.save().then(() => {
+        res.redirect('/get-items')
+    }).catch(err => console.log(err))
+
+})
+app.get('/items/:id', (req, res) => {
+    const id = req.params.id;
+    Item.findById(id).then(result => {
+        console.log('result', result);
+        res.render('item-detail', { item: result })
+    })
+})
+app.delete('/items/:id', (req, res) => {
+    const id = req.params.id;
+    Item.findByIdAndDelete(id).then(result => {
+        res.json({ redirect: '/get-items' })
+    })
+})
+app.put('/items/:id', (req, res) => {
+    const id = req.params.id;
+    Item.findByIdAndUpdate(id, req.body).then(result => {
+        res.json({ msg: 'Updated Successfully' })
+    })
 })
 
 app.use((req, res) => {
