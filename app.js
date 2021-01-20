a9e232a04248e5d71b967bcd001b2f697e8ad4cc
imrachu <rachitkanaujiakvs@gmail.com>
2021-01-21 01:25:35 +0530

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const Item = require('./models/items');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-const mongodb = 'mongodb+srv://ckmobile:ckmobile123@cluster0.niuuw.mongodb.net/item-database?retryWrites=true&w=majority';
+const mongodb = 'mongodb+srv://bidhayak:bidhayak@cluster0.lsfgf.mongodb.net/Hotel?retryWrites=true&w=majority';
 mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('connected')
     app.listen(3000);
